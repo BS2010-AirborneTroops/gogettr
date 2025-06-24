d9cc4b0d2cd29bbc89210c300b9c4376df6aff99
BS2010 <98692950+BS2010-AirborneTroops@users.noreply.github.com>
2025-06-25 01:36:12 +0700

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,5 @@
+"""Defines the GoGettr client module. Most of the useful functionality is
+inside PublicClient."""
+
+from gogettr.public import PublicClient  # noqa: F401
+from gogettr.private import PrivateClient
